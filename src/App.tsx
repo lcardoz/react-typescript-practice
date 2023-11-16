de1be57267531431679c9efd59536015fa965d9a
Leah Cardoz <leahcardoz@gmail.com>
2023-11-16 18:14:48 -0500

    created ListGroup component to display an array of mapped items

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import Message from './Message'
+import ListGroup from "./components/ListGroup";
 
 function App() {
 
   return (
-    <div>
-      <Message />
-    </div>
+    <>
+      <ListGroup />
+    </>
   )
 }
 
-export default App
+export default App;
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,13 +1,12 @@
-// rfc or rfce: shortcut to create functional component
+// rfce: shortcut to create functional component
 
 function Message() {
 
     const name = 'Leah'
 
     if (name)
-        return <h1>Hello {name}</h1>
+      return <h1>Hello {name}</h1>
     return <h1>Hello World</h1>
-
 }
 
 export default Message
