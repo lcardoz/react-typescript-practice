243d3c85d5a89300eb3531f9c79253494a7ed138
Leah Cardoz <leahcardoz@gmail.com>
2023-11-20 12:37:53 -0500

    practiced updating array with state

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import { useState } from "react";
 
 function App() {
-  const [customer, setCustomer] = useState({
-    name: "John",
-    address: {
-      city: "San Francisco",
-      zipCode: 94111,
-    },
-  });
+
+  const [tags, setTags] = useState(['happy', 'cheerful']);
 
   const handleClick = () => {
-    setCustomer({
-      ...customer,
-      address: { ...customer.address, zipCode: 94112 },
-    });
+    // Add item to array:
+    setTags([...tags, 'sad']);
+    
+    // Remove item from array, use filter method:
+    setTags(tags.filter(tag => tag !== 'happy'));
+
+    // Update array, use map method:
+    setTags(tags.map(tag => tag === 'happy' ? 'happiness' : tag))
   };
+  console.log(tags)
 
   return (
     <>
-      {customer.address.zipCode}
       <button onClick={handleClick}>Click Me!</button>
     </>
   );
