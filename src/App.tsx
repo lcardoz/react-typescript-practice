0c7ace5f0193033a40bba00c400e05983cefeea7
Leah Cardoz <leahcardoz@gmail.com>
2023-11-20 12:28:59 -0500

    practiced updating nested object state in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import Like from "./components/Like";
+import { useState } from "react";
 
 function App() {
+  const [customer, setCustomer] = useState({
+    name: "John",
+    address: {
+      city: "San Francisco",
+      zipCode: 94111,
+    },
+  });
+
+  const handleClick = () => {
+    setCustomer({
+      ...customer,
+      address: { ...customer.address, zipCode: 94112 },
+    });
+  };
+
   return (
     <>
-      <Like onClick={() => console.log('clicked')}/>
+      {customer.address.zipCode}
+      <button onClick={handleClick}>Click Me!</button>
     </>
   );
 }
