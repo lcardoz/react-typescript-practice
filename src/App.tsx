515a6afe342cdd0b62d57500d534b9a67b38521e
Leah Cardoz <leahcardoz@gmail.com>
2023-11-20 13:43:35 -0500

    installed Immer, imported produce to update state in the array of objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
 import { useState } from "react";
+import produce from "immer";
 
 function App() {
-
   const [bugs, setBugs] = useState([
-    { id: 1, title: 'Bug1', fixed: false },
-    { id: 2, title: 'Bug2', fixed: false },
-  ])
+    { id: 1, title: "Bug1", fixed: false },
+    { id: 2, title: "Bug2", fixed: false },
+  ]);
 
   const handleClick = () => {
-    setBugs(bugs.map(bug => bug.id === 1 ? { ... bug, fixed: true} : bug))
+    // setBugs(bugs.map(bug => bug.id === 1 ? { ... bug, fixed: true} : bug))
+
+    // we can also use Immer to update the object
+    setBugs(
+      produce((draft) => {
+        // draft is our array of bugs
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) bug.fixed = true;
+      })
+    );
   };
 
-  console.log(bugs)
+  console.log(bugs);
 
   return (
     <>
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title} {bug.fixed ? "Fixed" : "New"}'
+        </p>
+      ))}
       <button onClick={handleClick}>Click Me!</button>
     </>
   );
