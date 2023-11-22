a156399528eff472760b1a84029131cd610e7b25
Leah Cardoz <leahcardoz@gmail.com>
2023-11-22 13:53:04 -0500

    applied form validation with react-hook-form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,23 +1,19 @@
-// import { FormEvent, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 
+// so we can see properties of errors object in auto completion, and for type safety:
+interface FormData {
+  name: string;
+  age: number;
+}
+
 const Form = () => {
-  // const form = useForm()
-  // console.log(form)
-  // destructuring form object and grabbing register function:
-  const { register, handleSubmit } = useForm();
-  // call register and give it an argument which is the name of an input field, like name or age:
-  // console.log(register("name"));
-
-  // const [person, setPerson] = useState({
-  //   name: "",
-  //   age: "",
-  // });
-
-  // const handleSubmit = (e: FormEvent) => {
-  //   e.preventDefault();
-  //   console.log(person);
-  // };
+  
+  // destructuring form object and grabbing register function, with nested destructuring of formState; pass in our interface:
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
   // hover over data in handleSubmit function and see that data parameter has a data type of FieldValues:
   const onSubmit = (data: FieldValues) => console.log(data);
@@ -25,35 +21,36 @@ const Form = () => {
   return (
     // <form onSubmit={handleSubmit((data) => console.log(data))}> // change to below:
     <form onSubmit={handleSubmit(onSubmit)}>
-      {/* shortcut to generate code: div.mb-3>label.form-label+input.form-control + tab */}
-      {/* mb-3= margin bottom-3 (utility class in bootstrap) */}
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
         <input
-          // onChange={(e) => setPerson({ ...person, name: e.target.value })}
-          {...register("name")}
-          // value={person.name}
+          // validations: react-hook-form only calls handler function if form is valid:
+          {...register("name", { required: true, minLength: 2 })}
           id="name"
           type="text"
           className="form-control"
         />
+        {/* need ? after name: optional chaining, in case errors object is empty:  */}
+        {errors.name?.type === "required" && (
+          <p className="text-danger">The name field is required.</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">The name must be at least 2 characters.</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
         <input
-          // onChange={(e) => setPerson({ ...person, age: e.target.value })}
           {...register("age")}
-          // value={person.age}
           id="age"
           type="number"
           className="form-control"
         />
       </div>
-      {/* shortcut to generate code: button.btn.btn-primary + tab */}
       <button className="btn btn-primary" type="submit">
         Submit
       </button>
