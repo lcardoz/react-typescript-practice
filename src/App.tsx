dbce5569d6f27e3b4d0793a758fcd381efefcdec
Leah Cardoz <leahcardoz@gmail.com>
2023-11-27 17:33:11 -0500

    imported state for expense objects and set up delete logic to delete an expense and update the total

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
+import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
-  
-  const expenses = [
-    { id: 1, description: 'abc', amount: 10, category: 'Utilities' },
-    { id: 2, description: 'def', amount: 10, category: 'Utilities' },
-    { id: 3, description: 'ghi', amount: 10, category: 'Utilities' },
-    { id: 4, description: 'jkl', amount: 10, category: 'Utilities' },
-  ]
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "abc", amount: 10, category: "Utilities" },
+    { id: 2, description: "def", amount: 10, category: "Utilities" },
+    { id: 3, description: "ghi", amount: 10, category: "Utilities" },
+    { id: 4, description: "jkl", amount: 10, category: "Utilities" },
+  ]);
 
   return (
     <>
-      <ExpenseList expenses={expenses} onDelete={(id) => console.log('deleted:', id)} />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((expense) => expense.id !== id))}
+      />
     </>
   );
 }
